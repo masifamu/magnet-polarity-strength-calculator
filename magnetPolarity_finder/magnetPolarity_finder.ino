1246b8d2ee5c12917e5e68e5409c53103e5aaf1c
Mohammad Asif <69226688+masifamu@users.noreply.github.com>
2020-12-21 18:12:37 +0530

    This code fixes #1

--- magnetPolarity_finder/magnetPolarity_finder.ino
+++ magnetPolarity_finder/magnetPolarity_finder.ino
@@ -11,11 +11,19 @@ void loop() {
   // put your main code here, to run repeatedly:
   flux=(analogRead(analogPin)*4.951/1024.0);//*1000/1.8;
   flux=flux*1000/1.8;//into gauss
-  flux=flux/10;//in mT(miliTesla)
-  Serial.print("Flux : ");
-  Serial.print(flux);
-  Serial.print("mT");
-  if(flux > 150.0) Serial.println(" N-Ploe");
-  else if(flux < 110.0) Serial.println(" S-Pole");
-  else Serial.println(" ");
+  //flux=flux/10;//in mT(miliTesla)
+  
+  if(flux > 1330.0){
+    Serial.print("Flux : ");
+    Serial.print(flux);
+    Serial.print(" G");
+    Serial.println(" N");
+  }
+  else if(flux < 1280.0){
+    Serial.print("Flux : ");
+    Serial.print(flux);
+    Serial.print(" G");
+    Serial.println(" S");
+  }
+  else Serial.println("Align magnet with the sensor ");
 }
